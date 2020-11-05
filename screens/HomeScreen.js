b30bd822b0166b632848d7d9c340e864a396fdf2
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-11-05 20:50:18 +0500

    home init

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const HomeScreen = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
