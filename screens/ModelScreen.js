8af41e318163c13dce72696d8ebcd934d93c1adc
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-11-06 11:44:53 +0500

    fetch added

--- screens/ModelScreen.js
+++ screens/ModelScreen.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 
-const ModelScreen = (props) => {
+const ModelScreen = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.json())
+        .then(json => setData(json));
+    }, []);
+
     return(
         <View>
-            <Text>ModelScreen</Text>
+            <Text style={styles.text}>Name: {data.name}</Text>
+            <Text style={styles.text}>Email: {data.email}</Text>
+            <Text style={styles.text}>Username: {data.username}</Text>
         </View>
     )
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 25,
+        alignSelf: "center",
+        justifyContent: "center",
+
+    }
+});
 
 export default ModelScreen;
